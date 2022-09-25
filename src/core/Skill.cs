195de32a2269cf02ee8064f26570a03ae4e09db4
anthony <islasa1@users.noreply.github.com>
2022-09-25 13:12:01 -0700

    Making new fields

--- src/core/Skill.cs
+++ src/core/Skill.cs
@@ -14,18 +14,30 @@ public class Skill
     MISC
   }
 
-  public float exp_        =     0.0f;
+
+  public float exp  { get { return System.Math.Max( max_, exp_ + expprimary_ + expsecondary_ + expmisc_ ); } }
+
+
+  public float exp_          =     0.0f;
+  public float expprimary_   =     0.0f;
+  public float expsecondary_ =     0.0f;
+  public float expmisc_      =     0.0f;
+
   public float max_        = 10000.0f;
   public float primary_    =     5.0f;
   public float secondary_  =     2.5f;
   public float misc_       =     1.0f;
-  public float maxmisc_    =   500.0f;
+  public float maxsecondary_ =  2000.0f;
+  public float maxmisc_      =   500.0f;
 
   // Might not be used here
   // public static float LEVEL_STEP = 1.0 / 7.0;
 
   public string skillname_ = null;
   public SkillFactor factor_ = new SkillFactor();
+
+  // Not loaded or saved
+  ITreeAttribute     points_ = new TreeAttribute();
 
 
   /// default ctor
@@ -41,18 +53,25 @@ public class Skill
   {
     this.skillname_ = skillAttributes.GetString( "skillname" ) ?? defaultSkill.skillname_;
 
-    this.exp_       = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "exp"       ) ?? defaultSkill.exp_;
-    this.max_       = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "max"       ) ?? defaultSkill.max_;
-    this.primary_   = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "primary"   ) ?? defaultSkill.primary_;
-    this.secondary_ = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "secondary" ) ?? defaultSkill.secondary_;
-    this.misc_      = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "misc"      ) ?? defaultSkill.misc_;
-    this.maxmisc_   = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "maxmisc"   ) ?? defaultSkill.maxmisc_;
+    this.exp_          = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "exp"            ) ?? defaultSkill.exp_;
+    this.expprimary_   = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "expprimary"     ) ?? defaultSkill.expprimary_;
+    this.expsecondary_ = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "expsecondary"   ) ?? defaultSkill.expsecondary_;
+    this.expmisc_      = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "expmisc"        ) ?? defaultSkill.expmisc_;
+    this.max_          = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "max"            ) ?? defaultSkill.max_;
+    this.primary_      = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "primary"        ) ?? defaultSkill.primary_;
+    this.secondary_    = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "secondary"      ) ?? defaultSkill.secondary_;
+    this.misc_         = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "misc"           ) ?? defaultSkill.misc_;
+    this.maxsecondary_ = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "maxsecondary"   ) ?? defaultSkill.maxsecondary_;
+    this.maxmisc_      = TreeAtributeExtractor.tryGetAsType< float >( skillAttributes, "maxmisc"        ) ?? defaultSkill.maxmisc_;
 
     ITreeAttribute factorTree = skillAttributes.GetTreeAttribute( "factor" );
-    if ( factorTree != null )    
+    if ( factorTree != null )
     {
       this.factor_.readFromTree( factorTree, defaultSkill.factor_ );
     }
+
+    ITreeAttribute factorTree = skillAttributes.GetTreeAttribute( "points" );
+    if ( factorTree != null )
 
   }
 
@@ -64,7 +83,7 @@ public class Skill
   public float experienceLevel( )
   {
 
-    float relativeExperience = exp_ / max_;
+    float relativeExperience = exp / max_;
 
     // The first hyperbolic gets us into the 0->7/8 range, the next gets us (0->10/11)*1/8
     // Together when added we get *really* close to 8/8 but not quite while maintaining a hyperbolic shape
@@ -87,13 +106,17 @@ public class Skill
     TreeAttribute skillTree = new TreeAttribute();
 
     skillTree.SetString( "skillname", skillname_ );
-    
-    skillTree.SetFloat( "exp",       exp_       );
-    skillTree.SetFloat( "max",       max_       );
-    skillTree.SetFloat( "primary",   primary_   );
-    skillTree.SetFloat( "secondary", secondary_ );
-    skillTree.SetFloat( "misc",      misc_      );
-    skillTree.SetFloat( "maxmisc",   maxmisc_   );
+
+    skillTree.SetFloat( "exp",          exp_          );
+    skillTree.SetFloat( "expprimary",   expprimary_   );
+    skillTree.SetFloat( "expsecondary", expsecondary_ );
+    skillTree.SetFloat( "expmisc",      expmisc_      );
+    skillTree.SetFloat( "max",          max_          );
+    skillTree.SetFloat( "primary",      primary_      );
+    skillTree.SetFloat( "secondary",    secondary_    );
+    skillTree.SetFloat( "misc",         misc_         );
+    skillTree.SetFloat( "maxsecondary", maxsecondary_ );
+    skillTree.SetFloat( "maxmisc",      maxmisc_      );
     
     if ( getFactor )
     {
